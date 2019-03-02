14e0416c952042f2fa4069ad4f1ccb59001fee41
Semih Korkmaz <skorkmaz88@yahoo.com>
2019-03-02 21:58:49 +0100

    Create node_req.js

---
+++ node_req.js
@@ -0,0 +1,18 @@
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'http://dydra.com/skorkmaz/movielens1k/sparql',
+  headers: 
+   { 'cache-control': 'no-cache',
+     Authorization: 'Bearer YOUR_DYDRA_API_KEY',
+     'Content-Type': 'application/x-www-form-urlencoded',
+     Accept: 'application/sparql-results+json' },
+  form: 
+   { query: 'select * where { { graph ?g {?s ?p ?o} } union {?s ?p ?o} } limit 10',
+     undefined: undefined } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
